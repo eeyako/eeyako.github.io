bc0ecf723a5ea482694596f87daf7fa463cc4145
eeyako <eeyako42@gmail.com>
2024-10-05 20:39:22 -0700

    refactors javaScript code to use async/await instead of promises

--- index.js
+++ index.js
@@ -1,55 +1,79 @@
+const CMDS_ROOT_URL = 'https://help.autodesk.com/cloudhelp/2024/ENU/Maya-Tech-Docs/CommandsPython'
+const MEL_ROOT_URL = 'https://help.autodesk.com/cloudhelp/2024/ENU/Maya-Tech-Docs/Commands'
+const OM2_ROOT_URL = 'https://help.autodesk.com/cloudhelp/2024/ENU/MAYA-API-REF/py_ref'
+const OM_ROOT_URL = 'https://help.autodesk.com/cloudhelp/2024/ENU/MAYA-API-REF/cpp_ref'
+
+const title = document.querySelector(".title")
+const message = document.querySelector(".message")
+
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
 
-const cmdsRootUrl = "https://help.autodesk.com/cloudhelp/2024/ENU/Maya-Tech-Docs/CommandsPython"
-const melRootUrl = "https://help.autodesk.com/cloudhelp/2024/ENU/Maya-Tech-Docs/Commands"
-const omRootUrl = "https://help.autodesk.com/cloudhelp/2024/ENU/MAYA-API-REF/cpp_ref";
-const om2RootUrl = "https://help.autodesk.com/cloudhelp/2024/ENU/MAYA-API-REF/py_ref";
 
+async function fetchAndGo(query, rootUrl, index, selector) {
+    const lowerParam = query.toLowerCase()
+    const res = await fetch(`${rootUrl}/${index}`)
+    const htmlString = await res.text()
+    const obj = new DOMParser().parseFromString(htmlString, "text/html")
 
-function fetchAndGo(param, rootUrl, index, query) {
-    param = param.toLowerCase()
-    fetch(`${rootUrl}/${index}`)
-        .then(res => res.text())
-        .then(htmlString => new DOMParser().parseFromString(htmlString, "text/html"))
-        .then(obj => {
-            let a = obj.querySelectorAll(`${query}`);
-            for (let i = 0; i < a.length; ++i) {
-                if (param == a[i].innerHTML.toLowerCase()) {
-                    return a[i].getAttribute("href")
-                }
-            }
-        })
-        .then(url => window.location.replace(`${rootUrl}/${url}`));
+    const a = obj.querySelectorAll(selector)
+    for (let i = 0; i < a.length; i++) {
+        if (lowerParam == a[i].innerHTML.toLowerCase()) {
+            const url = a[i].getAttribute("href")
+
+            window.location.replace(`${rootUrl}/${url}`)
+            return 0
+        }
+    }
+    return 1
 }
 
-let title = document.querySelector(".title")
-let message = document.querySelector(".message")
 
-title.innerHTML = 'Loading...'
+async function handleUrlParams() {
+    const value = urlParams.entries().next()['value']
+    if (!value) {
+        title.innerHTML = 'Error: no query requested'
+        message.innerHTML = 'Available queries are: cmds, mel, om2, om'
+        return
+    }
 
-// Parse arguments
-if (urlParams.has('cmds')) {
-    let cmd = urlParams.get('cmds');
-    if (cmd) {
-        fetchAndGo(cmd, cmdsRootUrl, "index_all.html", ".related-links a");
+    let rootUrl
+    let index
+    let selector
+    const param = value[0]
+    const query = value[1]
+    switch (param) {
+        case 'cmds':
+            rootUrl = CMDS_ROOT_URL
+            index = 'index_all.html'
+            selector = '.related-links a'
+            break
+        case 'mel':
+            rootUrl = MEL_ROOT_URL
+            index = 'index_all.html'
+            selector = '.related-links a'
+            break
+        case 'om2':
+            rootUrl = OM2_ROOT_URL
+            index = 'annotated.html'
+            selector = 'a.el'
+            break
+        case 'om':
+            rootUrl = OM_ROOT_URL
+            index = 'annotated.html'
+            selector = 'a.el'
+            break
+        default:
+            title.innerHTML = 'Error: Incorrect query'
+            message.innerHTML = 'Available queries are: cmds, mel, om2, om'
+            return
     }
-} else if (urlParams.has('mel')) {
-    let mel = urlParams.get('mel');
-    if (mel) {
-        fetchAndGo(mel, melRootUrl, "index_all.html", ".related-links a");
+    const err = await fetchAndGo(query, rootUrl, index, selector)
+    if (err == 1) {
+        title.innerHTML = 'Not found'
+        message.innerHTML = `Could not find "${query}" under "${param}"`
     }
-} else if (urlParams.has('om2')) {
-    let om2 = urlParams.get('om2');
-    if (om2) {
-        fetchAndGo(om2, om2RootUrl, "annotated.html", "a.el")
-    }
-} else if (urlParams.has('om')) {
-    let om = urlParams.get('om');
-    if (om) {
-        fetchAndGo(om, omRootUrl, "annotated.html", "a.el")
-    }
-} else {
-    title.innerHTML = 'Incorrect query.'
-    message.innerHTML = 'Available queries are: cmds, mel, om2, om'
 }
+
+title.innerHTML = 'Searching...'
+handleUrlParams()
