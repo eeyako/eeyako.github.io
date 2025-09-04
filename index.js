bee4f6c5c38077c92a109316282513fc687e5f9c
eeyako <eeyako42@gmail.com>
2025-09-04 06:54:40 -0700

    adds support for qt search

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const CMDS_ROOT_URL = 'https://help.autodesk.com/cloudhelp/2024/ENU/Maya-Tech-Do
 const MEL_ROOT_URL = 'https://help.autodesk.com/cloudhelp/2024/ENU/Maya-Tech-Docs/Commands'
 const OM2_ROOT_URL = 'https://help.autodesk.com/cloudhelp/2024/ENU/MAYA-API-REF/py_ref'
 const OM_ROOT_URL = 'https://help.autodesk.com/cloudhelp/2024/ENU/MAYA-API-REF/cpp_ref'
+const QT_ROOT_URL = 'https://doc.qt.io/qt-6'
 
 const title = document.querySelector(".title")
 const message = document.querySelector(".message")
@@ -11,14 +12,13 @@ const urlParams = new URLSearchParams(queryString)
 
 
 async function fetchAndGo(query, rootUrl, index, selector) {
-    const lowerParam = query.toLowerCase()
     const res = await fetch(`${rootUrl}/${index}`)
     const htmlString = await res.text()
     const obj = new DOMParser().parseFromString(htmlString, "text/html")
 
     const a = obj.querySelectorAll(selector)
     for (let i = 0; i < a.length; i++) {
-        if (lowerParam == a[i].innerHTML.toLowerCase()) {
+        if (query == a[i].innerHTML.toLowerCase()) {
             const url = a[i].getAttribute("href")
 
             window.location.replace(`${rootUrl}/${url}`)
@@ -33,15 +33,15 @@ async function handleUrlParams() {
     const value = urlParams.entries().next()['value']
     if (!value) {
         title.innerHTML = 'Error: no query requested'
-        message.innerHTML = 'Available queries are: cmds, mel, om2, om'
+        message.innerHTML = 'Available queries are: cmds, mel, om2, om, qt'
         return
     }
 
     let rootUrl
     let index
     let selector
-    const param = value[0]
-    const query = value[1]
+    const param = value[0].toLowerCase()
+    const query = value[1].toLowerCase()
     switch (param) {
         case 'cmds':
             rootUrl = CMDS_ROOT_URL
@@ -63,6 +63,10 @@ async function handleUrlParams() {
             index = 'annotated.html'
             selector = 'a.el'
             break
+        case 'qt':
+            rootUrl = QT_ROOT_URL
+            window.location.replace(`${rootUrl}/${query}.html`)
+            return
         default:
             title.innerHTML = 'Error: Incorrect query'
             message.innerHTML = 'Available queries are: cmds, mel, om2, om'
@@ -74,6 +78,7 @@ async function handleUrlParams() {
         message.innerHTML = `Could not find "${query}" under "${param}"`
     }
 }
+
 
 title.innerHTML = 'Searching...'
 handleUrlParams()
